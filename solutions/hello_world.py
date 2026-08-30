c3720eb03f211321415d4b45b4b03d5b8e771fd5
abhinav <abhinavpadige06@gmail.com>
2026-08-30 16:10:17 +0530

    Add hello world Python file

---
+++ solutions/hello_world.py
@@ -0,0 +1,11 @@
+\"\"\"Hello World Python File.
+
+This file prints 'Hello, World!' to the console.
+\"\"\"
+
+def main():
+    \"\"\"Main function to print Hello World.\"\"\"
+    print("Hello, World!")
+
+if __name__ == "__main__":
+    main()
